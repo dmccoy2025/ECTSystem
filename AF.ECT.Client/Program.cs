4c582ea75de091b616125f636b813bcef8539fb1
Donell McCoy <donell.mccoy@divihn.com>
2025-10-08 16:09:57 -0400

    Configure WorkflowClient options from appsettings.json and update AssemblyInformationalVersion attributes

--- AF.ECT.Client/Program.cs
+++ AF.ECT.Client/Program.cs
@@ -10,14 +10,8 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddRadzenComponents();
 
-// Configure WorkflowClient options with default values
-builder.Services.Configure<WorkflowClientOptions>(options =>
-{
-    options.MaxRetryAttempts = 3;
-    options.InitialRetryDelayMs = 100;
-    options.MaxRetryDelayMs = 1000;
-    options.RequestTimeoutSeconds = 30;
-});
+// Configure WorkflowClient options from appsettings.json
+builder.Services.Configure<WorkflowClientOptions>(options => builder.Configuration.GetSection("WorkflowClient").Bind(options));
 
 // Register WorkflowClient for gRPC communication
 builder.Services.AddScoped<IWorkflowClient, WorkflowClient>();
--- AF.ECT.Server/obj/Debug/net9.0/AF.ECT.Server.AssemblyInfo.cs
+++ AF.ECT.Server/obj/Debug/net9.0/AF.ECT.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AF.ECT.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f568ebaf23f7836f2401dae310f2318d6adb4b26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be8ee5576d5793fcf3b829e8f8278da6ad7047f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AF.ECT.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AF.ECT.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
