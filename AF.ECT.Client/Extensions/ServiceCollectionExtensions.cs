e298895ce4740d7847e74012c6c008e4c958bc86
dmccoy2025 <dmccoy2025@outlook.com>
2025-10-17 20:22:55 -0400

    Add dependencies and HttpClient extension method
    Added `Radzen.Blazor` (v8.0.4) and `Blazored.LocalStorage` (v4.5.0) to the project. Included a project reference to `AF.ECT.Shared.csproj`. Removed the `Extensions` folder reference from the project file.
    
    Introduced a new `AddHttpClient` extension method in `ServiceCollectionExtensions.cs` under the `AF.ECT.Client.Extensions` namespace. This method registers a scoped `HttpClient` with its `BaseAddress` set to the `HostEnvironment.BaseAddress` for Blazor WebAssembly applications.

---
+++ AF.ECT.Client/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+namespace AF.ECT.Client.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHttpClient(this IServiceCollection services, WebAssemblyHostBuilder builder)
+    {
+        services.AddScoped(sp =>
+        {
+            return new HttpClient 
+            { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+            };
+        });
+
+        return services;
+    }
+}
