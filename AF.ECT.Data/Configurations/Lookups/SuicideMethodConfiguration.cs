d0abe55d52ca80d1adc284b5fb3eb353b5c8397c
dmccoy2025 <dmccoy2025@outlook.com>
2025-10-26 14:45:59 -0400

    feat(data): add EF Core entity configurations across multiple modules
    Add generated IEntityTypeConfiguration classes for many entities across Development, Reporting, Workflow, Lookups, Documents, Integration, and Reminders. Configure table/view mappings, primary keys, keyless/staging views, properties, relationships, and indexes for command structs, temporary staging tables, reporting query objects, workflow returns/RWOA, lookup/lookups, recently uploaded documents, SSIS configs, reminder settings, and related entities.

---
+++ AF.ECT.Data/Configurations/Lookups/SuicideMethodConfiguration.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using AF.ECT.Data.Entities;
+
+namespace AF.ECT.Data.Configurations.Lookups;
+
+/// <summary>
+/// Entity type configuration for the <see cref="SuicideMethod"/> entity.
+/// Configures the schema, table name, primary key, properties, and indexes for suicide method lookup.
+/// </summary>
+public class SuicideMethodConfiguration : IEntityTypeConfiguration<SuicideMethod>
+{
+    /// <summary>
+    /// Configures the entity of type <see cref="SuicideMethod"/>.
+    /// </summary>
+    /// <param name="builder">The builder to be used to configure the entity type.</param>
+    public void Configure(EntityTypeBuilder<SuicideMethod> builder)
+    {
+        builder.ToTable("SuicideMethod", "dbo");
+
+        // Primary Key
+        builder.HasKey(e => e.Id);
+
+        // Properties
+        builder.Property(e => e.Id)
+            .HasColumnName("ID");
+
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(e => e.Active);
+
+        // Indexes
+        builder.HasIndex(e => e.Active)
+            .HasDatabaseName("IX_SuicideMethod_Active");
+
+        builder.HasIndex(e => e.Name)
+            .HasDatabaseName("IX_SuicideMethod_Name");
+    }
+}
---
+++ AF.ECT.Data/Configurations/Workflow/RwoaConfiguration.cs
@@ -0,0 +1,87 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using AF.ECT.Data.Entities;
+
+namespace AF.ECT.Data.Configurations.Workflow;
+
+/// <summary>
+/// Entity type configuration for the <see cref="Rwoa"/> entity.
+/// Configures the schema, table name, primary key, properties, and relationships for RWOA (Return Without Action) tracking.
+/// </summary>
+public class RwoaConfiguration : IEntityTypeConfiguration<Rwoa>
+{
+    /// <summary>
+    /// Configures the entity of type <see cref="Rwoa"/>.
+    /// </summary>
+    /// <param name="builder">The builder to be used to configure the entity type.</param>
+    public void Configure(EntityTypeBuilder<Rwoa> builder)
+    {
+        builder.ToTable("RWOA", "dbo");
+
+        // Primary Key
+        builder.HasKey(e => e.RwoaId);
+
+        // Properties
+        builder.Property(e => e.RwoaId)
+            .HasColumnName("RWOAId");
+
+        builder.Property(e => e.RefId)
+            .HasColumnName("RefID");
+
+        builder.Property(e => e.Workstatus)
+            .HasColumnName("workstatus");
+
+        builder.Property(e => e.Workflow)
+            .HasColumnName("workflow");
+
+        builder.Property(e => e.SentTo)
+            .HasMaxLength(50)
+            .HasColumnName("sentTo");
+
+        builder.Property(e => e.ReasonSentBack)
+            .HasColumnName("reasonSentBack");
+
+        builder.Property(e => e.ExplanationForSendingBack)
+            .HasColumnType("text")
+            .HasColumnName("explanationForSendingBack");
+
+        builder.Property(e => e.Sender)
+            .HasMaxLength(50)
+            .HasColumnName("sender");
+
+        builder.Property(e => e.DateSent)
+            .HasColumnType("datetime")
+            .HasColumnName("dateSent");
+
+        builder.Property(e => e.CommentsBackToSender)
+            .HasColumnType("text")
+            .HasColumnName("commentsBackToSender");
+
+        builder.Property(e => e.DateSentBack)
+            .HasColumnType("datetime")
+            .HasColumnName("dateSentBack");
+
+        builder.Property(e => e.CreatedBy)
+            .HasColumnName("createdBy");
+
+        builder.Property(e => e.CreatedDate)
+            .HasColumnType("datetime")
+            .HasColumnName("createdDate");
+
+        builder.Property(e => e.Rerouting)
+            .HasColumnName("rerouting");
+
+        // Indexes
+        builder.HasIndex(e => e.RefId)
+            .HasDatabaseName("IX_RWOA_RefID");
+
+        builder.HasIndex(e => e.Workflow)
+            .HasDatabaseName("IX_RWOA_Workflow");
+
+        builder.HasIndex(e => e.DateSent)
+            .HasDatabaseName("IX_RWOA_DateSent");
+
+        builder.HasIndex(e => e.CreatedBy)
+            .HasDatabaseName("IX_RWOA_CreatedBy");
+    }
+}
